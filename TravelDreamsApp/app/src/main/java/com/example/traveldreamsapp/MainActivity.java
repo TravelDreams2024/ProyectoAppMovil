42dd0970b495c98f7f2784e9649420d1f489e2d4
Facundo Cascallares <facundocascallares@hotmail.com>
2024-09-21 01:41:10 -0300

    Agrego activity

--- TravelDreamsApp/app/src/main/java/com/example/traveldreamsapp/MainActivity.java
+++ TravelDreamsApp/app/src/main/java/com/example/traveldreamsapp/MainActivity.java
@@ -1,8 +1,6 @@
 package com.example.traveldreamsapp;
 
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -13,12 +11,14 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        // Ajuste de los insets para adaptarse a los bordes
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
     }
 }
